ae6a3d8a74bfb55595d0c02f458bd7285aa27230
vandris <andras.vranyecz@ddc.sze.hu>
2026-05-13 14:14:16 +0200

    db_inject module

--- src/csv_parser/parser.py
+++ src/csv_parser/parser.py
@@ -1,16 +1,39 @@
-import json
 import sqlite3
+import os
 
 
-def parse_to_db(file_path):
-    file = open(file_path)
-    con = sqlite3.connect("data.db")
-    cur = con.cursor()
-    cur.execute(f"CREATE TABLE message")
-    for i in file[0]:
-        cur.execute(f"ALTER TABLE message ADD {i}")
-    for line in file:
-        for i in line:
-            cur.execute(f"INSERT INTO message VALUES {i}")
+"""def parse(file_path):
+    with open(file_path) as file:
+    
+    return header, data"""
 
-parse_to_db(".testfiles/test.csv")
+def db_inject(schema, data=""):
+    with sqlite3.connect('data.db') as con:
+        cursor = con.cursor()
+        columns_with_types = [f"{col_name} {constraints}" for col_name, constraints in schema.items()]
+        attributes = ', '.join(columns_with_types)
+        cursor.execute(f'CREATE TABLE message({attributes})')
+        placeholders = ', '.join(['?'] * len(schema))
+        cursor.execute(f'INSERT INTO message VALUES ({placeholders})', data)
+        print('injection successful')
+        print('table contents:')
+        statement = '''SELECT * FROM message'''
+        cursor.execute(statement)
+        output = cursor.fetchall()
+        for i in output:
+            print(i)
+    con.close()
+
+if os.path.exists('data.db'):
+    os.remove('data.db')
+
+table_schema = {
+    'id': 'INTEGER PRIMARY KEY',  
+    'date': 'DATETIME NOT NULL',      
+    'message': 'TEXT NOT NULL'             
+}
+
+row_to_insert = (1, '2026-01-01', 'első üzenet')
+
+
+db_inject(table_schema, row_to_insert)
